028484659419a4d3bfe8e1127088dbfc9a15c07d
lubianat <tiagolubiana@hotmail.com>
2019-08-06 18:52:22 -0300

    traduzindo rbind

--- R/brincando_com_tabelas.R
+++ R/brincando_com_tabelas.R
@@ -18,3 +18,27 @@
 cola.colunas <- function(...){
   cbind(...)
 }
+
+
+#' cola.linhas
+#'
+#' Combina tabelas e tabelas,
+#' ou tabelas e vetore, ou até vetores e vetores,
+#' colando as linhas do segundo objeto no fim
+#' (à direita) do primeiro objeto.
+#'
+#' Gera uma tabela no fim.
+#'
+#' Wrapper da função cbind.
+#'
+#'  @param  ... vetores ou matrizes
+#'  @export
+#'  @example
+#'  m <- cola.linhas(1, 1:7)
+#'  m
+
+cola.linhas <- function(...){
+  rbind(...)
+}
+
+
